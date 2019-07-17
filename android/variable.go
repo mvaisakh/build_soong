AospExtended/platform_build_soong

soong: extend with aosp board flags
Includes the following commit:

Author: Simon Shields <simon@lineageos.org>
Date:   Mon Sep 4 21:09:44 2017 +1000

    soong: explicitly specify name of AOSP variables struct

    Change-Id: Ic754b8a13658ddc40620f985581bd1754b780b15

Change-Id: If127c9da6b57340ae57e0cf725151cdcc3c82ef3

android/variable.go
@@ -20,6 +20,8 @@ import (
 	"runtime"
 	"strings"
 
+	"aosp/soong/android_aosp"
+
 	"github.com/google/blueprint/proptools"
 )
 
@@ -114,6 +116,9 @@ type variableProperties struct {
 			Static_libs  []string
 			Srcs         []string
 		}
+
+		// include Aosp variables
+		Aosp android_aosp.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -230,6 +235,9 @@ type productVariables struct {
 	PgoAdditionalProfileDirs []string `json:",omitempty"`
 
 	VendorVars map[string]map[string]string `json:",omitempty"`
+
+	// include Aosp variables
+	Aosp android_aosp.ProductVariables
 }
 
 func boolPtr(v bool) *bool {

mvaisakh/build_soong

Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

android/variable.go
@@ -20,6 +20,8 @@ import (
 	"runtime"
 	"strings"
 
+	"aosp/soong/android"
+
 	"github.com/google/blueprint/proptools"
 )
 
@@ -114,6 +116,9 @@ type variableProperties struct {
 			Static_libs  []string
 			Srcs         []string
 		}
+
+		// include Unlegacy variables
+		*android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -230,6 +235,9 @@ type productVariables struct {
 	PgoAdditionalProfileDirs []string `json:",omitempty"`
 
 	VendorVars map[string]map[string]string `json:",omitempty"`
+
+	// include Unlegacy variables
+	*android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {